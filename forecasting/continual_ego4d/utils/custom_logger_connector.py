393f38112def4f8017f347a29462ec1bd99c9834
mattdl-meta <matthiasdelange@fb.com>
2022-08-17 12:16:43 -0700

    Logger fix to log on final batch

--- forecasting/continual_ego4d/utils/custom_logger_connector.py
+++ forecasting/continual_ego4d/utils/custom_logger_connector.py
@@ -7,7 +7,11 @@ class CustomLoggerConnector(LoggerConnector):
 
     @property
     def should_update_logs(self) -> bool:
+        """
+        Log on
+        - initial 0th step (we overwrite 'self.trainer.global_step+1' to 'self.trainer.global_step')
+        - last training batch in epoch
+        """
         should_log_every_n_steps = (self.trainer.global_step) % self.trainer.log_every_n_steps == 0
-        return should_log_every_n_steps or self.trainer.should_stop
-
+        return should_log_every_n_steps or self.trainer.is_last_batch
 
