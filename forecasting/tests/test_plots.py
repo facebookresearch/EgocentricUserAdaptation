83aafa9b5188077e4fae50a5af875a2ba7c908c5
mattdl-meta <matthiasdelange@fb.com>
2022-08-27 16:10:37 -0700

    unittest plotting

---
+++ forecasting/tests/test_plots.py
@@ -0,0 +1,44 @@
+import unittest
+
+from continual_ego4d.metrics.past_metrics import ConditionalOnlineForgettingMetric
+from collections import defaultdict
+import matplotlib.pyplot as plt
+
+
+class PlotConditionalOnlineForgettingMetricCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.metric = ConditionalOnlineForgettingMetric(
+            base_metric_mode='acc', action_mode="action",
+            keep_action_results_over_time=True, do_plot=True,
+
+            # Optional
+            main_metric_name="TESTCASE_METRIC",
+            current_batch_cond_set=False,
+            in_cond_set=False,
+            k=1,
+        )
+
+        self.metric.action_results_over_time = {
+            "prev_batch_idx": defaultdict(list),  # <action, list(<prev_iter,current_iter,delta_value>)
+            "current_batch_idx": defaultdict(list),
+            "delta": defaultdict(list),
+        }
+
+        for action, prev_batch_idx, current_batch_idx, delta in [
+            (0, 1, 2, 10), (1, 1, 2, 20), (3, 1, 20, 30), (10, 15, 20, -10)
+        ]:
+            self.metric.action_results_over_time['prev_batch_idx'][action].append(prev_batch_idx)
+            self.metric.action_results_over_time['current_batch_idx'][action].append(current_batch_idx)
+            self.metric.action_results_over_time['delta'][action].append(delta)
+
+    def test_plotting(self):
+        fig_dict = self.metric.plot()
+
+        for figure in fig_dict.values():
+            figure.show()
+            plt.clf()
+
+
+if __name__ == '__main__':
+    unittest.main()
