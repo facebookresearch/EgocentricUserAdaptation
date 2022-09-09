8e6ef46fb628919ba4793e0d4ac1ed9a2f596f3e
mattdl-meta <matthiasdelange@fb.com>
2022-09-09 10:13:10 -0700

    Summarizing streams cleanup: run_summarize_streams.py

--- forecasting/continual_ego4d/run_summarize_streams.py
+++ forecasting/continual_ego4d/run_summarize_streams.py
@@ -2,33 +2,25 @@
 As in-clip sequential sampling occurs on runtime, this script collects all samples and pickles them for further analysis
 in the notebooks.
 """
+import copy
 import sys
 
 from continual_ego4d.utils.checkpoint_loading import load_pretrain_model, load_meta_state, save_meta_state, PathHandler
 import pickle
+from collections import defaultdict
 
-import pprint
-import concurrent.futures
-from collections import deque
 import torch
 from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint, DeviceStatsMonitor, GPUStatsMonitor, Timer
-from pytorch_lightning.plugins import DDPPlugin
-from pytorch_lightning.loggers import CSVLogger, TensorBoardLogger
-from continual_ego4d.utils.custom_logger_connector import CustomLoggerConnector
 
 from ego4d.utils import logging
 from ego4d.utils.parser import load_config, parse_args
-from ego4d.tasks.long_term_anticipation import MultiTaskClassificationTask
 
 from continual_ego4d.tasks.continual_action_recog_task import ContinualMultiTaskClassificationTask
 from continual_ego4d.datasets.continual_action_recog_dataset import extract_json
 
-from scripts.slurm import copy_and_run_with_config
 import os
 import os.path as osp
-import shutil
-from continual_ego4d.datasets.continual_action_recog_dataset import Ego4dContinualRecognition
+from continual_ego4d.datasets.continual_dataloader import construct_trainstream_loader
 
 
 def main(cfg):
@@ -38,7 +30,6 @@ def main(cfg):
         cfg.OUTPUT_DIR = cfg.RESUME_OUTPUT_DIR  # Resume run if specified, and output to same output dir
     print(f"Output is redirected to: {cfg.OUTPUT_DIR}")
     os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-    path_handler = PathHandler(cfg)
 
     # Assertion bypassing
     cfg.SOLVER.ACCELERATOR = "gpu"
@@ -59,26 +50,23 @@ def main(cfg):
     checkpoint_filename = f"dataset_entries_{cfg.DATA.USER_SUBSET}_{osp.basename(data_path).split('.')[0]}.ckpt"
     checkpoint_path = osp.join(cfg.OUTPUT_DIR, checkpoint_filename)
     print(f"Dataset checkpoint path={checkpoint_path}")
+    assert not osp.isfile(checkpoint_path), "Not overwriting summary checkpoint"
 
-    if osp.isfile(checkpoint_path):
-        datasets = torch.load(checkpoint_path)
-    else:
-        datasets = {}
-        for user_id in all_user_ids_s:
-            print(f"Collecting dataset for user {user_id}")
-            datasets[user_id] = collect_user_dataset(cfg, user_id, user_datasets[user_id], path_handler)
-        print(f"Collected all user datasets, USERS={list(datasets.keys())}")
+    datasets = {}
+    for user_id in all_user_ids_s:
+        print(f"Collecting dataset for user {user_id}")
+        datasets[user_id] = collect_user_dataset(copy.deepcopy(cfg), user_id, user_datasets[user_id])
+    print(f"Collected all user datasets, USERS={list(datasets.keys())}")
 
-        # torch.save(datasets, checkpoint_path)
-        with open(checkpoint_path, 'wb') as f:
-            pickle.dump(datasets, f)
+    # torch.save(datasets, checkpoint_path)
+    with open(checkpoint_path, 'wb') as f:
+        pickle.dump(datasets, f)
 
 
 def collect_user_dataset(
         cfg,
         user_id: str,
         user_dataset,
-        path_handler: PathHandler,
 ) -> (str, bool):
     """ Run single user sequentially with sequential ClipSampler, return data list of annotation entries returned by the clip sampler."""
     seed_everything(cfg.RNG_SEED)
@@ -86,24 +74,18 @@ def collect_user_dataset(
     # Set user configs
     cfg.DATA.COMPUTED_USER_ID = user_id
     cfg.DATA.COMPUTED_USER_DS_ENTRIES = user_dataset
-    cfg.COMPUTED_USER_DUMP_FILE = path_handler.get_user_streamdump_file(user_id)  # Dump-path for Trainer stream info
-    # cfg.USER_RESULT_PATH = path_handler.get_user_results_dir(user_id)
 
-    # Choose task type based on config.
-    if cfg.DATA.TASK == "continual_classification":
-        task = ContinualMultiTaskClassificationTask(cfg)
-
-    else:
-        raise ValueError(f"cfg.DATA.TASK={cfg.DATA.TASK} not supported")
-    print(f"Initialized task as {type(task)}")
-
-    dataset_obj: Ego4dContinualRecognition = task.train_dataloader().dataset
+    loader = construct_trainstream_loader(cfg)
+    dataset_obj = loader.dataset
     dataset_tuple_entries = dataset_obj.seq_input_list
 
     # Unpack entries and leave out video_paths in (video_path, entry) tuples
-    dataset_entries = [t[1] for t in dataset_tuple_entries]
+    final_entries = []
+    for video_path, video_entry_dict in dataset_tuple_entries:
+        video_entry_dict['video_path'] = video_path
+        final_entries.append(video_entry_dict)
 
-    return dataset_entries
+    return final_entries
 
 
 if __name__ == "__main__":
