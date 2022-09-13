41256fd06e32b5b2f899220f985d93c8986e22ab
mattdl-meta <matthiasdelange@fb.com>
2022-09-13 13:30:15 -0700

    Final exps scripts final01_01

---
+++ exps/ego4d_action_recog/final01_01_finetuning_sgd/grid.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+
+#BASE_LR: 1e-4
+#LR_POLICY: constant
+#MAX_EPOCH: 1
+#MOMENTUM: 0.9
+#OPTIMIZING_METHOD: sgd
+
+# For sgd
+grid_cfg_names="SOLVER.BASE_LR,SOLVER.MOMENTUM,SOLVER.NESTEROV" # Split by comma
+grid_overwrite_args=""
+
+val_idx=2
+gridvals=( "1e-1" "1e-2" "1e-3")
+grid_arg="SOLVER.BASE_LR ${gridvals[${val_idx}]}"
+grid_overwrite_args+=" ${grid_arg}"
+
+val_idx=2
+gridvals=( "0." "0.3" "0.6" "0.9" )
+grid_arg="SOLVER.MOMENTUM ${gridvals[${val_idx}]}"
+grid_overwrite_args+=" ${grid_arg}"
+
+val_idx=0
+gridvals=( True False )
+grid_arg="SOLVER.NESTEROV ${gridvals[${val_idx}]}"
+grid_overwrite_args+=" ${grid_arg}"
+
+
+# Grid specific resources
+grid_overwrite_args+=" GPU_IDS 3 NUM_USERS_PER_DEVICE 2" # 0,7
+
+# Report final
+echo "grid_overwrite_args=$grid_overwrite_args"
+
+# Run script in current dir (same process with source)
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${__dir}/ego4d_action_recog.sh" "${grid_cfg_names}" "${grid_overwrite_args}"
