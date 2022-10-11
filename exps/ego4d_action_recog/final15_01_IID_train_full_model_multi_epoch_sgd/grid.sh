16f40f8dc3ad942e7a274a2b0d4849c2fca03aba
mattdl-meta <matthiasdelange@fb.com>
2022-10-11 11:01:05 -0700

    Multi-epoch IID baseline

---
+++ exps/ego4d_action_recog/final15_01_IID_train_full_model_multi_epoch_sgd/grid.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# For sgd
+grid_cfg_names="SOLVER.BASE_LR" # Split by comma
+grid_overwrite_args=""
+
+val_idx=1 # TODO RUN
+gridvals=( "1e-1" "1e-2" "1e-3")
+grid_arg="SOLVER.BASE_LR ${gridvals[${val_idx}]}"
+grid_overwrite_args+=" ${grid_arg}"
+
+# Grid specific resources
+grid_overwrite_args+=" GPU_IDS 6 NUM_USERS_PER_DEVICE 5 GRID_RESUME_LATEST False" # 0,7
+
+# Report final
+echo "grid_overwrite_args=$grid_overwrite_args"
+
+# Run script in current dir (same process with source)
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${__dir}/ego4d_action_recog.sh" "${grid_cfg_names}" "${grid_overwrite_args}"
