ad695059ad02b265f374e5c5e25b27f84115d119
mattdl-meta <matthiasdelange@fb.com>
2022-10-13 20:30:28 -0700

    exps update

---
+++ exps/ego4d_action_recog/script_wrapper_run/run_adhoc_processing.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Provide checkpoint path as argument if you want to resume
+
+#-----------------------------------------------------------------------------------------------#
+# Add ego4d code modules to pythonpath
+this_script_dirpath=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P) # Change location to current script
+root_path=${this_script_dirpath}/../../../                                    # One dir up
+ego4d_code_root="$root_path/forecasting"
+export PYTHONPATH=$PYTHONPATH:$ego4d_code_root
+
+# Unique run id
+timestamp=$(date '+%Y-%m-%d_%H-%M-%S')
+uuid=$(uuidgen)
+run_id="${timestamp}_UID${uuid}"
+echo "RUN-ID=${run_id}"
+#-----------------------------------------------------------------------------------------------#
+# PATHS
+#-----------------------------------------------------------------------------------------------#
+
+python -m continual_ego4d.processing.run_adhoc_metric_processing_wandb
+
