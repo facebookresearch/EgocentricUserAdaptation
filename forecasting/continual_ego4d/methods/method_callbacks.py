157e7b2c0391ab019bcfe17a4e07f082a49d4766
mattdl-meta <matthiasdelange@fb.com>
2022-10-26 20:41:31 -0700

    BatchedLabelWindowPredictor

--- forecasting/continual_ego4d/methods/method_callbacks.py
+++ forecasting/continual_ego4d/methods/method_callbacks.py
@@ -718,6 +718,117 @@ class LabelWindowPredictor(Method):
 
 
 @METHOD_REGISTRY.register()
+class BatchedLabelWindowPredictor(LabelWindowPredictor):
+    """
+    Uses the previous batch iteration to predict current batch samples.
+    This is different from the previous <batch-size> preceding samples of each sample in the LabelWindowPredictor!
+    BatchedLabelWindowPredictor gives a fair comparison with methods to see how much they fit to the current batch.
+    """
+    run_predict_before_train = False
+
+    def __init__(self, cfg, lightning_module):
+        super().__init__(cfg, lightning_module)
+
+        # Empty task metrics
+        lightning_module.past_metrics = []
+        lightning_module.future_metrics = []
+        # lightning_module.current_batch_metrics = [] # Get ACC etc for current batch
+        lightning_module.continual_eval_freq = -1
+        lightning_module.plotting_log_freq = -1
+        logger.debug(f"Reset all metrics to empty.")
+
+        # Overwrite dataloaders
+        assert cfg.DATA.RETURN_VIDEO is False, \
+            f"Must set cfg.DATA.RETURN_VIDEO=False for efficient loading of stream labels only"
+        assert lightning_module.train_loader.dataset.return_video is False  # Only return labels
+        lightning_module.predict_loader = None
+
+        # States
+        self.window_size = cfg.ANALYZE_STREAM.WINDOW_SIZE_SAMPLES  # Determines length of window in samples
+        assert self.window_size > 0
+
+        # Memory history
+        self.label_window: deque[tuple] = deque()  # Each window is a list of action-tuples
+
+        # For pred creation
+        self.verbs_count, self.nouns_count = cfg.MODEL.NUM_CLASSES  # Verbs, nouns
+
+    def training_first_forward(self, inputs_t, labels_t, current_batch_stream_idxs: list, *args, **kwargs):
+        """ For each sample, use the WINDOW-SIZE preceding samples for prediction distr.
+        As action is predicted as 2 independent classifiers (verbs,nouns), we should normalize predictions for each
+        distribution. This also means we should count the verbs/nouns separately to form these distributions. """
+        loss = None
+        log_results = {}
+
+        batch_idx = self.lightning_module.stream_state.batch_idx
+        batch_size = self.lightning_module.train_loader.batch_size  # Retrieve like this for last batch remain cst
+        assert self.window_size == batch_size, f"Only supports to use the exact batch size."
+
+        # Get prediction tensors based on previous batch
+        pred_verb_t = torch.zeros(self.verbs_count).to(labels_t.device)
+        pred_noun_t = torch.zeros(self.nouns_count).to(labels_t.device)
+        if batch_idx > 0:
+            assert len(self.label_window) > 0
+
+            verbs_in_window = [a[0] for a in self.label_window]
+            nouns_in_window = [a[1] for a in self.label_window]
+
+            for verb_idx, noun_idx in zip(verbs_in_window, nouns_in_window):
+                pred_verb_t[verb_idx] += 1
+                pred_noun_t[noun_idx] += 1
+
+            # Normalize
+            pred_verb_tn = pred_verb_t / sum(pred_verb_t)
+            pred_noun_tn = pred_noun_t / sum(pred_noun_t)
+
+        elif batch_idx == 0:  # If empty window, predict uniform
+            pred_verb_tn = pred_verb_t.fill_(1 / self.verbs_count)
+            pred_noun_tn = pred_noun_t.fill_(1 / self.verbs_count)
+
+        else:
+            raise ValueError("Negative batch idx")
+
+        # ADD samples only after predicting with previous batch: Current bath not influenced
+        # Get formatted (non-tensor) label
+        for current_batch_stream_idx in current_batch_stream_idxs:
+            action_label: tuple[int, int] = self.lightning_module.stream_state.sample_idx_to_action_list[
+                current_batch_stream_idx
+            ]
+            self.label_window.append(action_label)  # Add to window
+
+        # Pop if oversize
+        while len(self.label_window) > self.window_size:
+            self.label_window.popleft()
+
+        # Concat preds
+        verb_preds = pred_verb_tn.unsqueeze(0).expand(len(current_batch_stream_idxs), -1)
+        noun_preds = pred_noun_tn.unsqueeze(0).expand(len(current_batch_stream_idxs), -1)
+        preds = (verb_preds, noun_preds)
+
+        # Update
+        empty = [None] * len(current_batch_stream_idxs)
+        self._update_stream_tracking(
+            stream_sample_idxs=current_batch_stream_idxs,
+            new_batch_feats=empty,
+            new_batch_verb_pred=preds[0],
+            new_batch_noun_pred=preds[1],
+            new_batch_action_loss=empty,
+            new_batch_verb_loss=empty,
+            new_batch_noun_loss=empty,
+        )
+
+        return loss, preds, log_results
+
+    def training_update_loop(self, loss_first_fwd, inputs, labels, current_batch_stream_idxs: list, *args, **kwargs):
+        """ No updates made. """
+        pass
+
+    def prediction_step(self, inputs, labels, stream_sample_idxs: list, *args, **kwargs) \
+            -> Tuple[Tensor, List[Tensor], Dict]:
+        raise NotImplementedError("Should not call prediction for this method.")
+
+
+@METHOD_REGISTRY.register()
 class HindsightLabelWindowPredictor(Method):
     """
     Use last batch-size samples as prediction distribution.
