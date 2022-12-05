dbbd1b7aeb811f2337be4aebd56391bcaa50f463
mattdl <de.lange.matthias@gmail.com>
2022-12-05 19:07:39 +0100

    cleanup files removing

--- src/Ego4D-Future-Hand-Prediction/slowfast/datasets/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-from .ego4dhand import Ego4dhand
-from .build import DATASET_REGISTRY, build_dataset  # noqa
-
