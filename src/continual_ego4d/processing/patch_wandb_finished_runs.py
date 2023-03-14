231767ea294268905ec662015f87f5630b4dfe9a
mattdl <de.lange.matthias@gmail.com>
2023-03-14 10:14:06 +0100

    patch_wandb_finished_runs.py: final comments

--- src/continual_ego4d/processing/patch_wandb_finished_runs.py
+++ src/continual_ego4d/processing/patch_wandb_finished_runs.py
@@ -3,10 +3,11 @@ Patch for runs that have a csv dump that marks the end of a stream training, but
 This script iterates runs and if csv dump exists, the run is finished, add this to the wandb entry.
 """
 
-import pandas as pd
-import wandb
-import pprint
 import os
+import pprint
+
+import wandb
+
 from continual_ego4d.processing.utils import get_group_names_from_csv, get_group_run_iterator
 
 api = wandb.Api()
@@ -15,14 +16,14 @@ api = wandb.Api()
 PROJECT_NAME = "ContinualUserAdaptation"
 
 # Adapt settings
-UPDATE_ALL_WANDB_RUNS = False # Iterate all WandB runs in project
+iterate_all_wandb_runs = False # Iterate all WandB runs in project
 csv_path = '/your/path/to/wandb_export_2022-09-19T12_08_04.153-07_00.csv'  # Iterate all WandB runs of the groupnames in this csv
 single_group_name = None  # Or set to Groupname, e.g. "Finetuning_2022-10-14_13-11-14_UIDac6a2798-7fb0-4e9c-9896-c6c54de5237c"
 
 
 def main():
-    if UPDATE_ALL_WANDB_RUNS:
-        print("Updating all runs")
+    if iterate_all_wandb_runs:
+        print(f"Updating all runs in project: {PROJECT_NAME}")
         # Update all group entries:
         for idx, user_run in enumerate(api.runs(PROJECT_NAME)):
             update_finished_run_entry(idx, user_run)
