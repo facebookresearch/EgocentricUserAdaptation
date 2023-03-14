304e73504de9eac6b6f401163dc844ff0b2bbde1
mattdl <de.lange.matthias@gmail.com>
2023-03-14 10:08:17 +0100

    run_adhoc_add_finished_true_wandb.py

--- src/continual_ego4d/processing/run_adhoc_add_finished_true_wandb.py
+++ src/continual_ego4d/processing/run_adhoc_add_finished_true_wandb.py
@@ -1,6 +1,6 @@
 """
-RUN REMOTE ONLY!
-Iterate runs and if csv dump exists, the run is finished, add this to the wandb entry.
+Patch for runs that have a csv dump that marks the end of a stream training, but no WandB finished_run=True.
+This script iterates runs and if csv dump exists, the run is finished, add this to the wandb entry.
 """
 
 import pandas as pd
@@ -11,19 +11,14 @@ from continual_ego4d.processing.utils import get_group_names_from_csv, get_group
 
 api = wandb.Api()
 
+# Fixed settings
+PROJECT_NAME = "ContinualUserAdaptation"
+
 # Adapt settings
 UPDATE_ALL_WANDB_RUNS = False
 REMOTE = False
-
-csv_filename = 'wandb_export_2022-09-19T12_08_04.153-07_00.csv'  # TODO copy file here and past name here
-single_group_name = None
-single_group_name = "Finetuning_2022-10-14_13-11-14_UIDac6a2798-7fb0-4e9c-9896-c6c54de5237c"
-
-# Fixed settings
-PROJECT_NAME = "matthiasdelange/ContinualUserAdaptation"
-remote_csv_dirname = '/home/matthiasdelange/sftp_remote_projects/ContextualOracle_Matthias/adhoc_results'  # Move file in this dir
-local_csv_dirname = '/home/mattdl/projects/ContextualOracle_Matthias/adhoc_results'  # Move file in this dir
-csv_dirname = remote_csv_dirname if REMOTE else local_csv_dirname
+csv_path = '/your/path/to/wandb_export_2022-09-19T12_08_04.153-07_00.csv'  # TODO copy file here and past name here
+single_group_name = None  # Or set to Groupname, e.g. "Finetuning_2022-10-14_13-11-14_UIDac6a2798-7fb0-4e9c-9896-c6c54de5237c"
 
 
 def main():
@@ -32,7 +27,7 @@ def main():
             selected_group_names: list[str] = [single_group_name]
         else:
             print("Updating only groups in csv")
-            selected_group_names_csv_path = os.path.join(csv_dirname, csv_filename)
+            selected_group_names_csv_path = csv_path
             assert os.path.isfile(selected_group_names_csv_path)
             selected_group_names: list[str] = get_group_names_from_csv(selected_group_names_csv_path)
         print(f"Group names={pprint.pformat(selected_group_names)}")
@@ -40,7 +35,7 @@ def main():
         # Iterate groups (a collective of independent user-runs)
         finished_user_ids = []
         for group_name in selected_group_names:
-            for idx, user_run in enumerate(get_group_run_iterator(PROJECT_NAME, group_name, finished_runs_only=False)): # TODO be carefull will indicate duplicate runs also as finished!
+            for idx, user_run in enumerate(get_group_run_iterator(PROJECT_NAME, group_name, finished_runs_only=False)):
                 finished_user_id = update_finished_run_entry(idx, user_run)
                 if finished_user_id is not None:
                     finished_user_ids.append(finished_user_id)
@@ -57,10 +52,6 @@ def update_finished_run_entry(entry_idx, user_run):
     user_id = user_run.config['DATA.COMPUTED_USER_ID']
     parent_outdir = user_run.config['OUTPUT_DIR']
     user_dump_path = os.path.join(parent_outdir, 'user_logs', f"user_{user_id}", 'stream_info_dump.pth')
-
-    if user_id == '436':
-        import pdb;
-        pdb.set_trace()
 
     if os.path.isfile(user_dump_path):
         user_run.summary['finished_run'] = True
