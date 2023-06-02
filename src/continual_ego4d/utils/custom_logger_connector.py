ea90118a4456a736b2bfb9b424117dcf9ba07b00
Michael Iuzzolino <mliuzzolino@meta.com>
2023-06-02 00:33:21 -0700

    Update custom_logger_connector.py

--- src/continual_ego4d/utils/custom_logger_connector.py
+++ src/continual_ego4d/utils/custom_logger_connector.py
@@ -1,3 +1,7 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 from pytorch_lightning.trainer.connectors.logger_connector.logger_connector import LoggerConnector
 
 
