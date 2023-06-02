a95828cf70a45c1b8bab3eeb54471de1c68de865
Michael Iuzzolino <mliuzzolino@meta.com>
2023-06-02 00:43:16 -0700

    Update lr_scheduler.py

--- src/ego4d/optimizers/lr_scheduler.py
+++ src/ego4d/optimizers/lr_scheduler.py
@@ -1,3 +1,7 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 from . import optimizer as optim
 from torch.optim.lr_scheduler import LambdaLR, CosineAnnealingLR
 import math 
