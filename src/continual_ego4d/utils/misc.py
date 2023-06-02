fd9fbad8b3d0a33d4aa4957c6586b3e6ba7531f1
Michael Iuzzolino <mliuzzolino@meta.com>
2023-06-02 00:33:46 -0700

    Update misc.py

--- src/continual_ego4d/utils/misc.py
+++ src/continual_ego4d/utils/misc.py
@@ -1,3 +1,7 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 """ This script should have no dependencies on other ego4d/continual_ego4d to avoid circular imports."""
 import os
 from fvcore.common.config import CfgNode
