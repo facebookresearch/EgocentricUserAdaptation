928002e00804d250dc75dd64a0e39435941c9dc2
Michael Iuzzolino <mliuzzolino@meta.com>
2023-06-02 00:32:03 -0700

    Update run.sh

--- reproduce/test_user_results/LWP_batched_hindsight/run.sh
+++ reproduce/test_user_results/LWP_batched_hindsight/run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
 # This script runs the labeled window predictor (LWP), operating per batch (batched).
 # The hindsight performance predicts for the full stream using only the final batch in the stream.
 
