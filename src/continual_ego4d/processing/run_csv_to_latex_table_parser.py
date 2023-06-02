b950f070be31f4422586e98e77be6e42f34dd966
Michael Iuzzolino <mliuzzolino@meta.com>
2023-06-02 00:36:50 -0700

    Update run_csv_to_latex_table_parser.py

--- src/continual_ego4d/processing/run_csv_to_latex_table_parser.py
+++ src/continual_ego4d/processing/run_csv_to_latex_table_parser.py
@@ -1,3 +1,7 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 """
 Takes WandB table CSV export (e.g. download from the GUI), and transfers to formatted Latex table.
 All the required metrics per table are enlisted in this script, and should be present in the CSV (can be selected in WandB).
