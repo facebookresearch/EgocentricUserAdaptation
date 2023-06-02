81f8287152e290a5b4225b6c327832b670e03536
Michael Iuzzolino <mliuzzolino@meta.com>
2023-06-02 00:37:24 -0700

    Update run_summarize_user_streams.py

--- src/continual_ego4d/processing/run_summarize_user_streams.py
+++ src/continual_ego4d/processing/run_summarize_user_streams.py
@@ -1,3 +1,7 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 """
 As sequential sampling within the action label time-ranges occurs at runtime for the user-streams,
 this script collects all samples and pickles them for further analysis in the notebooks.
